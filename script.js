ccfe52db1c023c24c1e190a48bef9944e1c87b22
Pacheco67 <166664111+ViniciusPacheco67@users.noreply.github.com>
2025-06-18 09:16:54 -0300

    tela de login

---
+++ script.js
@@ -0,0 +1,20 @@
+function Clicar() {
+    var email = document.getElementById('iemail').value
+    var senha = document.getElementById('isenha').value
+    var resposta = document.getElementById('resposta').value
+
+    alert(`Olá ${email}, seja bem-vindo!`)
+    alert(`Oi ${senha}shhhhhhhhi 🤫`)
+
+    if (email.length < 10 || !email.includes('@')) {
+    resposta.innerHTML = '<p style="color: red;">Email inválido</p>';
+    return;
+    }
+
+    if (senha.length < 8) {
+        resposta.innerHTML = '<p style="color: red;">Senha deve ter pelo menos 8 caracteres</p>';
+        return;
+    }
+
+    resposta.innerHTML = `<p style="color: green;">Olá <strong>${email}</strong>, seja bem-vindo!</p>`;
+}
